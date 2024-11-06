1622bc3723284a45b7aa27b0d9df58e4aacd7ce3
oldjeff131 <oldjeff131@gmail.com>
2024-11-06 12:11:59 +0800

    first commit

---
+++ client2.c
@@ -0,0 +1,22 @@
+#include<sys/socket.h>
+#include<netinet/in,h>
+#include<string.h>
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+int main(){
+    struct sockaddr_in server;
+    int sock;
+    char buf[]="";
+    bzero(&server,sizeof(sserver));
+    server.sin_family=PF_INET;
+    server.sin_addr.s_addr=inet_addr("127.0.0.1");
+    server.s_in_port=htons(5678);
+    sock=socket(PF_INET,SOCK_STREAM,0);
+    connect(sock,(struct sockaddr*)&server, sizeof(server));
+    while{
+    send(sock,buf,sizeof(buf),0);
+    printf("Send Message： %s\n",buf);
+    }(buf!='00')
+    close(sock);
+}
